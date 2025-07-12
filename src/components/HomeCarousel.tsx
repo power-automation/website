eb5f44af107bf0b5c6f2f291cebfd3cb7a670c9e
= <=>
2025-07-12 15:22:42 +0530

    fixed UI in mobile view

--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -1,27 +1,33 @@
 "use client";
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+
 export default function HomeCarousel() {
-    const images = ['1.jpg', '2.jpg', '3.jpg','4.jpg'];
-    const [currentIndex, setCurrentIndex] = React.useState(0);
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIndex(prev => (prev === images.length - 1 ? 0 : prev + 1));
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [images.length]);
+  const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) =>
+        prev === images.length - 1 ? 0 : prev + 1
+      );
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
     <div className="relative w-full max-w-xl mx-auto">
-      <div className="relative w-full h-[500px]">
+      <div className="relative w-full aspect-[16/9] md:h-[500px]">
         {images.map((img, idx) => (
           <img
             key={img}
             src={`/carousel/${img}`}
             alt={`Slide ${idx + 1}`}
-            style={{ height: '500px', objectFit: 'cover' }}
-            className={`absolute top-0 left-0 w-full h-full rounded-lg transition-opacity duration-500 ${idx === currentIndex ? 'opacity-100 z-10' : 'opacity-0 z-0'}`}
+            className={`absolute top-0 left-0 w-full h-full object-fit rounded-lg transition-opacity duration-500 ${
+              idx === currentIndex ? "opacity-100 z-10" : "opacity-0 z-0"
+            }`}
           />
         ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 export default function Navbar() {
   return (
-    <div className="bg-amber-500/70 text-white border-b-1 border-neutral-700">
+    <div className="bg-gray-800 text-white border-b-1 border-neutral-700">
       <div className="flex items-center md:hidden">        
         <Image src="/logo.png" alt="Logo" width={60} height={40} className='rounded-full my-5 ml-5' />
         <div className='text-2xl p-3 w-[90%] text-center font-bold'>Power Automation</div>  
